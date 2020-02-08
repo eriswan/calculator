7bf67e143a6e8ec71cb56a161eecd28f71662b9b
Erika <erikaalves018@gmail.com>
2020-02-08 20:01:59 -0300

    Adding five fragments

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,26 +1,36 @@
 package com.example.calculator
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        navView.setOnNavigationItemSelectedListener {
+            val fragment = when (it.itemId) {
+                R.id.tab_sum -> SumFragment()
+                R.id.tab_sub -> SubFragment()
+                R.id.tab_div -> DivFragment()
+                R.id.tab_mult -> MultFragment()
+                R.id.tab_about -> AboutFragment()
+                else -> null
+            }
+
+            fragment?.let {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.nav_host_fragment, it)
+                    .commit()
+            }
+
+            true
+        }
+
+        navView.selectedItemId = R.id.tab_sum
     }
 }
